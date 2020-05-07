16787c79178dbb47a2e65ffa84a3cf7301c640f2
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-05-07 11:50:32 -0700

    wip: almost got output parsing to work

--- ortholang_kernel/kernel.py
+++ ortholang_kernel/kernel.py
@@ -11,8 +11,7 @@ import urllib
 
 from IPython.display import display, Image
 
-OL_WELCOME = u'Welcome to the OrthoLang interpreter!'
-OL_PROMPT  = u' —▶ '
+OL_ARROW  = u'—▶'
 OL_BYENOW  = u'Bye for now!'
 OL_CFGFILE = '/home/jefdaj/ortholang_kernel/test1.cfg'
 OL_LOGFILE = '/tmp/ortholang_kernel.log' # TODO add it to the main syslog
@@ -22,8 +21,54 @@ HANDLER = LOGGING.FileHandler(OL_LOGFILE)
 HANDLER.setFormatter(LOGGING.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s'))
 
 LOGGER = LOGGING.getLogger('ortholang_kernel')
-LOGGER.setLevel(LOGGING.DEBUG)
+LOGGER.setLevel(LOGGING.DEBUG) # TODO turn down
 LOGGER.addHandler(HANDLER)
+
+LOGGER.debug('reading ortholang_kernel.py...')
+
+#def ensure_trailing_newline(txt):
+    #return txt.strip()
+#     try:
+#         if txt[-1] == '\n':
+#             return txt
+#         else:
+#             return txt + '\n'
+#     except:
+#         return txt + '\n'
+
+# from https://www.tutorialspoint.com/How-can-I-remove-the-ANSI-escape-sequences-from-a-string-in-python
+def remove_ansi_escapes(txt):
+    ansi_escape =re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
+    return ansi_escape.sub('', txt)
+
+# def remove_regex(regex, txt):
+#     return re.sub(regex, '', txt) # TODO flags: DOTALL?
+
+# TODO use custom prompt from the class?
+def remove_prompt(txt):
+    prompt = re.compile('(\r|\n)*.*?' + OL_ARROW)
+    return prompt.sub('', txt)
+
+def collapse_newlines(txt):
+    return re.sub('(\r|\n){2,}', '\n', txt)
+
+def clean_lines(txt):
+    LOGGER.debug('clean_lines txt: "%s"' % txt)
+    # txt = remove_regex('(\r?\n)?\r?\n(ortholang|.*?\.ol)$', txt) # TODO put the ortholang part here instead?
+    # txt = remove_regex('ortholang$', txt) # TODO adjust for script?
+    # lines = txt.splitlines() # TODO does \ need escaping?
+    # lines = [remove_ansi_escapes(l) for l in lines]
+    # lines = clean_lines(lines)
+    # txt = '\n'.join(lines)
+    # lines = [l for l in lines if not is_prompt(l)]
+    # txt = '\n'.join(lines)
+    # lines = lines[:-1] # TODO does this ever fail?
+
+    txt = remove_ansi_escapes(txt)
+    txt = remove_prompt(txt)
+    #txt = collapse_newlines(txt) # TODO remove this?
+    LOGGER.debug('clean_lines result: "%s"' % txt)
+    return txt
 
 def contains_plot(txt):
     return 'plot image "' in txt
@@ -45,31 +90,25 @@ class OrthoLangKernel(Kernel):
         args = ["--config", OL_CFGFILE, "--interactive"]
         LOGGER.info('spawning ortholang %s' % str(args))
         self.ol_process = spawn('ortholang', args, encoding=ENCODING, echo=False, timeout=None)
-        self.ol_process.expect(OL_WELCOME)
+        self.ol_process.expect_exact(self.ol_prompt)
         LOGGER.debug("before: '%s'" % self.ol_process.before)
         LOGGER.debug("after: '%s'" % self.ol_process.after)
         LOGGER.info('REPL should be ready for input')
 
     def __init__(self, *args, **kwargs):
         LOGGER.debug('OrthoLangKernel.__init__')
+        self.ol_prompt = OL_ARROW + ' ' # TODO adjust when loading a script
         self.spawn_repl()
         super(OrthoLangKernel, self).__init__(*args, **kwargs)
 
-    def cleanup_lines(self, lines):
-        # remove the prompt
-        lines = lines[:-1]
-        # remove any blank lines
-        while True:
-            if not len(lines[-1].strip()) == 0:
-                break
-            lines = lines[:-1]
-        return lines
+#     def remove_prompt(self, txt):
+#         return txt.replace(self.ol_prompt, '').strip()
 
-    def prepare_content(self, txt):
-        lines = txt.split('\n')
-        lines = self.cleanup_lines(lines)
-        txt = '\n'.join(lines)
-        return txt
+#     def prepare_content(self, txt):
+#         lines = txt.split('\n')
+#         lines = clean_lines(lines)
+#         txt = '\n'.join(lines)
+#         return txt
 
     def load_plots(self, txt):
         regex = u'\[?plot image "(.*?)"'
@@ -84,18 +123,98 @@ class OrthoLangKernel(Kernel):
         return plots
 
     def do_execute(self, code, silent, store_history=True, user_expressions=None, allow_stdin=False):
+
+        # There's one extra rule (TODO is it really extra?) for parsing in the
+        # notebook interface: when a cell contains multiple statements, they
+        # must be separated by blank lines.  For example the blank lines here
+        # are important:
+        #
+        # > test1 = ["one",
+        # >          "two"]
+        # >
+        # > test2 = ["two", "three"]
+        # >
+        # > :show
+        #
+        # Otherwise we have no obvious way to know how many prompts to expect
+        # without re-implementing the OrthoLang parser in Python!
+
         LOGGER.debug("OrthoLangKernel.do_execute: '%s'" % code)
 
-        self.ol_process.sendline(code + '\n')
+        # Break into statements based on blank lines
+        # from https://stackoverflow.com/a/27003351/429898
+        statements = [[]]
+        for line in code.splitlines():
+            if len(line) == 0:
+                if len(statements[-1]) > 0:
+                    statements.append([])
+            else:
+                statements[-1].append(line)
+        LOGGER.debug("statements: '%s'" % statements)
 
-        options = [OL_PROMPT, OL_BYENOW]
-        self.ol_process.expect(options)
-        index = self.ol_process.expect(options)
+        # Run them individually
+        statements = ['\n'.join(s) for s in statements]
+
+        outputs = []
+        for s in statements:
+            outputs.append(self.do_execute_statement(s))
+        LOGGER.debug("statement outputs: '%s'" % outputs)
+
+        # TODO remove? maybe there needs to be one extra one here
+        # self.ol_process.expect_exact(options)
+
+        output = ''.join(outputs)#  + '\n'
+
+        # nlines = len(code.splitlines())
+        # LOGGER.debug('got %d lines of code, so expect %d prompts' % (nlines, nlines))
+
+        # self.ol_process.sendline(code + '\n')
+        # naecrs = len(self.ol_process.before.find('\x1b[0K'))
+        # LOGGER.debug('found %d cursor-right escape codes' % naecrs)
+
+        # options = [OL_ARROW, OL_BYENOW] # TODO what do we do if the user :quits? restart?
+
+        # this works around a bug where multiple statements per cell causes multiple prompts to be printed
+        # output = ''
+#         while True:
+#             self.ol_process.expect_exact(options)
+#             before = self.remove_prompt(self.ol_process.before)
+#             LOGGER.debug("before: '%s'" % before)
+#             if len(before) == 0:
+#                 break
+#             output += before
+#             LOGGER.debug("captured output so far: '%s'" % output)
+#             # LOGGER.debug("before: '%s'" % self.ol_process.before)
+#             LOGGER.debug("after: '%s'" % self.ol_process.after)
+#             # TODO rethink this: what would the repl example look like for when you want to return vs not? draw it out
+#             #      oh here's an idea: is output only going to be 'ortholang' over and over until the last one?
+#             #      another idea: the final line should be one that DOES NOT end in a newline yet
+#             # if len(self.ol_process.before.split('\n')) < 2:
+#             # if self.ol_process.before.strip() == u'ortholang':
+#             #     break
+
+        # this is hacky, but more or less works to process a multi-statement cell
+        # we assume that for n lines of input, there will be n prompts
+        # TODO warn users that only the last line of a cell should print output
+#         lines = []
+#         while True:
+#             self.ol_process.expect_exact(options)
+#             before = clean_lines(self.ol_process.before)
+#             LOGGER.debug("before: '%s'" % before)
+#             lines += before
+#             LOGGER.debug("lines so far: '%s'" % lines)
+#             if len(before) < 3:
+#                 break
+
+        # self.ol_process.expect_exact(options)
+        # output = clean_output(self.ol_process.before)
+        # output = '\n'.join(lines)
+        # LOGGER.debug("cleaned output: '%s'" % output)
 
         if not silent:
-            txt = self.prepare_content(self.ol_process.before)
-            if contains_plot(txt):
-                plots = self.load_plots(txt) # TODO return the others
+            # txt = self.prepare_content(output)
+            if contains_plot(output):
+                plots = self.load_plots(output) # TODO return the others
                 for plot in plots:
                     content = {
                         # note: 'data' key renamed to 'text' in messaging protocol 5.0
@@ -110,21 +229,32 @@ class OrthoLangKernel(Kernel):
                     self.send_response(self.iopub_socket, 'display_data', content)
                     LOGGER.debug('ok')
             else:
-                content = {'name': 'stdout', 'text': txt}
+                content = {'name': 'stdout', 'text': output}
                 self.send_response(self.iopub_socket, 'stream', content)
 
         return {'status': 'ok',
                 # The base class increments the execution count
-                'execution_count': self.execution_count,
+                'execution_count': self.execution_count, # TODO is this messed up by the statements thing?
                 'payload': [],
                 'user_expressions': {},
                }
+
+    def do_execute_statement(self, code):
+        #code = ensure_trailing_newline(code)
+        code = code.strip()
+        LOGGER.debug("do_execute_statement '%s'" % code)
+        self.ol_process.sendline(code)
+        options = [self.ol_prompt, OL_BYENOW] # TODO what do we do if the user :quits? restart?
+        self.ol_process.expect_exact(options)
+        output = clean_lines(self.ol_process.before + self.ol_process.after)
+        LOGGER.debug("statement output: '%s'" % output)
+        return output
 
     def quit_repl(self):
         try:
             LOGGER.info('quitting repl...')
             self.ol_process.sendline(':quit\n')
-            self.ol_process.expect(OL_BYENOW, timeout=5)
+            self.ol_process.expect_exact(OL_BYENOW, timeout=5)
             LOGGER.debug('final output: "%s"' % (self.ol_process.before + self.ol_process.after))
             self.ol_process.kill(0)
             self.ol_process.close(force=True)
@@ -145,4 +275,4 @@ if __name__ == '__main__':
     from ipykernel.kernelapp import IPKernelApp
     IPKernelApp.launch_instance(kernel_class=OrthoLangKernel)
 
-# LOGGER.debug('finished without syntax errors?')
+LOGGER.debug('finished without syntax errors')
