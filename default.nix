a42e2cd1b2771726b7605c81162b16a7542a9f33
Jeffrey David Johnson <jefdaj@berkeley.edu>
2020-05-13 10:35:42 -0700

    wip on default.nix

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
-# The iPython kernel examples are all packaged as Python modules, but it seems
-# that what we really want is a wrapped binary? Maybe that's just more
-# convenient to set up without Nix via modules.
+# This looks a little weird because OrthoLang includes its own pinned nixpkgs
+# for reproduciblity, and there's no obvious reason we would want that one to
+# be out of sync with the one used to build the Jupyter kernel. So we reach
+# into the ortholang submodule and use its nixpkgs settings. That behavior can
+# be overridden by passing a different pkgs argument from your system config.
 
-{ pkgs           ? (import <nixpkgs> {})
-, ortholang      ? (pkgs.callPackage ./ortholang { inherit pkgs; })
+# TODO doCheck needs $HOME set to run correctly?
+
+{ ortholang      ? import ./ortholang
+, pkgs           ? import ./ortholang/nixpkgs
 , pythonPackages ? pkgs.python37Packages
 }:
 
@@ -21,11 +25,6 @@ pythonPackages.buildPythonApplication rec {
     imageio
     numpy
   ];
-  doCheck = false; # TODO needs to set $HOME to run correctly?
-  inherit ortholang;
-  postInstall = ''
-    for f in $out/bin/*; do
-      wrapProgram $f --prefix PATH : "${ortholang}/bin"
-    done
-  '';
+  makeWrapperArgs = ["--prefix PATH : ${ortholang}/bin"];
+  doCheck = false;
 }
