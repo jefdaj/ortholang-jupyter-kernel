029b9e25953a0ce9bf1c9ddebdcb73d43a907233
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-11-06 12:27:19 -0700

    wip: arion jupyterServer

--- arion-compose.nix
+++ arion-compose.nix
@@ -1,38 +1,24 @@
-let
-
-  ortholang = import ./ortholang;
-  pkgs      = import ./ortholang/nixpkgs;
-  ortholang-jupyter-kernel = pkgs.callPackage ./default.nix rec {
-    inherit pkgs ortholang;
-    pythonPackages = pkgs.python3Packages;
-  };
-
-in {
+{
   services.jupyterServer = { config, pkgs, ... }: {
-    service.useHostStore = true;
+
+    service.capabilities.SYS_ADMIN = true;
+    service.useHostStore = true; # TODO does this work with systemd?
     service.ports = [
-      "8001:8888" # host:container TODO use 80 in the container?
+      "8889:8889" # host:container TODO use 80 in the container?
     ];
-    service.command = [ config.nixos.build.run-jupyter ];
+
+    nixos.useSystemd = true; # TODO remove?
 
     nixos.configuration  = { config, pkgs, ... }: {
       boot.isContainer = true;
-      system.build.run-jupyter = pkgs.writeScript "run-jupyter" ''
-        $!{pkgs.coreutils}/bin/bash
-        ${pkgs.coreutils}/bin/mkdir -p /home/jupyter
-        PATH='${config.systemd.services.jupyter.environment.PATH}'
-        ${config.systemd.services.jupyter.runner}
-      '';
+      users.users.jupyter.isSystemUser = true; # TODO remove?
 
-      users.users.jupyter = {
-        isNormalUser = true;
-      };
       # jupyter server based on:
       # https://old.reddit.com/r/NixOS/comments/e4hl0r/problems_setting_up_jupyterserver_with_nginxproxy/
       services.jupyter = {
-        enable = true; # disabled so i can test it separately
-        notebookDir = "/home/jupyter"; # TODO figure this out
-        port = 8888;
+        enable = true;
+        notebookDir = "/var/lib/jupyter/lab"; # TODO is this a good place?
+        port = 8889;
         ip = "127.0.0.1";
     
         # generated with `jupyter notebook password`,
@@ -51,13 +37,26 @@ in {
           c.IPKernelApp.matplotlib = 'inline'
         '';
     
-        kernels = {
-          ortholang = {
-            displayName = "OrthoLang 0.9.5 (2020-05-15)";
-            argv = [ "${ortholang-jupyter-kernel}/bin/ortholang_jupyter_kernel" "-f" "{connection_file}" ];
-            language = "ortholang";
+        kernels =
+
+          # TODO get this working with niv instead
+          # TODO try switching to jupyterWith without systemd?
+          let
+            pinnedOrtholang = import ./ortholang;
+            pinnedNixpkgs   = import ./ortholang/nixpkgs;
+            kernel = pinnedNixpkgs.callPackage ./default.nix rec {
+              ortholang      = pinnedOrtholang;
+              pkgs           = pinnedNixpkgs;
+              pythonPackages = pinnedNixpkgs.python37Packages;
+            };
+          in {
+            ortholang = {
+              displayName = "OrthoLang 0.9.5 (2020-05-15)";
+              argv = [ "${kernel}/bin/ortholang_jupyter_kernel" "-f" "{connection_file}" ];
+              language = "ortholang";
+            };
           };
-        };
+
       };
     };
   };
