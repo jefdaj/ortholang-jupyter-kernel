b43d02a253a8592425433654d920e733985b2d4a
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-11-06 14:56:32 -0700

    add pinned ortholang, but no use yet

--- arion-compose.nix
+++ arion-compose.nix
@@ -16,16 +16,29 @@ let
       kernels = [ iPython ];
   };
 
+  # pinned ortholang kernel v0.9.5
+  # TODO build kernel with current nixpkgs instead of matching ortholang?
+  # TODO get all this from niv of course
+  pinnedOrtholang = import ./ortholang;
+  pinnedNixpkgs   = import ./ortholang/nixpkgs;
+  kernel = pinnedNixpkgs.callPackage ./default.nix rec {
+    ortholang      = pinnedOrtholang;
+    pkgs           = pinnedNixpkgs;
+    pythonPackages = pinnedNixpkgs.python37Packages;
+  };
+ 
 in {
   config.services = {
 
-    jupyter = {
+    jupyterWithOrholang = {
       service.useHostStore = true;
 
       # TODO don't allow root?
       service.command = [ "${pkgs.bashInteractive}/bin/bash" "-c" ''
         ${pkgs.coreutils}/bin/mkdir -p "$$LAB_ROOT"
         ${pkgs.coreutils}/bin/chown jupyter:jupyter "$$LAB_ROOT" -R
+        echo "including ${pinnedOrtholang} just to show it builds"
+        echo "including ${kernel} just to show it builds"
         cd "$$LAB_ROOT"
         ${jupyterEnvironment}/bin/jupyter-lab --allow-root --no-browser --log-level=DEBUG --ip=0.0.0.0
       '' ];
