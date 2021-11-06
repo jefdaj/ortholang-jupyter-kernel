591323a8ec2aa86d4d5f4b20f1563cd55a339e39
Jeffrey David Johnson <jefdaj@protonmail.ch>
2021-11-06 13:14:04 -0700

    test arion + jupyterWith + python kernel

--- arion-compose.nix
+++ arion-compose.nix
@@ -1,22 +1,42 @@
 { pkgs, ... }:
-{
+
+let
+
+  # from https://github.com/tweag/jupyterWith shell.nix example
+  jupyter = import (builtins.fetchGit {
+    url = https://github.com/tweag/jupyterWith;
+    rev = "";
+  }) {};
+  iPython = jupyter.kernels.iPythonWith {
+    name = "python";
+    packages = p: with p; [ numpy ];
+  };
+  jupyterEnvironment =
+    jupyter.jupyterlabWith {
+      kernels = [ iPython ];
+  };
+
+in {
   config.services = {
 
-    webserver = {
+    jupyterWithPython = {
       service.useHostStore = true;
-      service.command = [ "${pkgs.bashInteractive}/bin/sh" "-c" ''
-                            cd "$$WEB_ROOT"
-                            ${pkgs.python3}/bin/python -m http.server
-                          ''
-                        ];
+
+      # TODO don't allow root?
+      service.command = [ "${pkgs.bashInteractive}/bin/bash" "-c" ''
+        ${pkgs.coreutils}/bin/mkdir -p "$$LAB_ROOT"
+        cd "$$LAB_ROOT"
+        ${jupyterEnvironment}/bin/jupyter-lab --allow-root --no-browser --log-level=DEBUG --ip=0.0.0.0
+      '' ];
       service.ports = [
 
-        # python server defaults to 8000,
-        # but we need to switch to avoid a conflict on the host
-        "7000:8000" # host:container
+        # 8888 is the default jupyter lab port
+        # but here we change the host side to avoid messing with the system version
+        "8888:8888" # host:container
 
       ];
-      service.environment.WEB_ROOT = "${pkgs.nix.doc}/share/doc/nix/manual";
+      # TODO where would this be ideally?
+      service.environment.LAB_ROOT = "/var/lib/jupyter/lab";
     };
   };
 }
